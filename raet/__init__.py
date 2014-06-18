830a08ed88a55455df6678cb2967bada4d8beb4b
Pedro Algarvio <pedro@algarvio.me>
2014-06-18 09:31:56 +0100

    Allow RAET to be PIP installable. Fixes #15
    Created a new module which holds the package metadata, `__version__`,
    `__author__`, `__license__`. Additionally added `__version_info__` which allows
    math based version comparison.
    This metadata is still available in `raet.__init__` because we import it there,
    but more importantly, we can now load this metadata in the setup script without
    having to load the raet module(`raet.__init__`, which in turn loads all it's
    modules which in turn load the dependencies). This way, we allow pip to install
    the required dependencies before actually needing then.

--- raet/__init__.py
+++ raet/__init__.py
@@ -4,13 +4,12 @@ raet modules
 
 __init__.py file for raet package
 '''
-__version__ = "0.0.19"
-__author__ = "Samuel M. Smith"
-__license__ =  "Apache2"
-
 
 __all__ = ['raeting', 'nacling', 'keeping', 'lotting', 'stacking', 'road', 'lane']
 
 import  importlib
 for m in __all__:
     importlib.import_module(".{0}".format(m), package='raet')
+
+# Load the package metadata
+from raet.__metadata__ import *
