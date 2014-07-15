d1f353e7affe21435e0ed273c84c292d41133981
Samuel M Smith <smith.samuel.m@gmail.com>
2014-07-15 16:31:15 -0600

    fix race condition with try except

--- raet/lane/yarding.py
+++ raet/lane/yarding.py
@@ -7,6 +7,7 @@ yarding.py raet protocol estate classes
 # Import python libs
 import socket
 import os
+import errno
 
 # Import ioflo libs
 from ioflo.base.odicting import odict
@@ -84,12 +85,25 @@ class Yard(lotting.Lot):
                 except OSError as ex:
                     dirpath = os.path.abspath(os.path.expanduser(ALT_YARD_UXD_DIR))
                     if not os.path.exists(dirpath):
-                        os.makedirs(dirpath)
+                        try:
+                            os.makedirs(dirpath)
+                        except OSError as ex:
+                            if ex.errno == errno.EEXIST:
+                                pass # race condition
+                            else:
+                                raise
             else:
                 if not os.access(dirpath, os.R_OK | os.W_OK):
                     dirpath = os.path.abspath(os.path.expanduser(ALT_YARD_UXD_DIR))
                     if not os.path.exists(dirpath):
-                        os.makedirs(dirpath)
+                        try:
+                            os.makedirs(dirpath)
+                        except OSError as ex:
+                            if ex.errno == errno.EEXIST
+                                pass # race condition
+                            else:
+                                raise
+
             ha = os.path.join(dirpath, "{0}.{1}.uxd".format(self.lanename, self.name))
 
         self.ha = ha
