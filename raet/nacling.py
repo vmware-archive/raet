03df7b016e0066ea1ecf4615698b3fa383ed56c2
Samuel M Smith <smith.samuel.m@gmail.com>
2014-09-11 10:43:30 -0600

    Fixed uuid on windows to use time.clock

--- raet/nacling.py
+++ raet/nacling.py
@@ -4,6 +4,7 @@ nacling.py raet protocol nacl (crypto) management classes
 '''
 
 # Import python libs
+import sys
 import time
 import binascii
 import six
@@ -544,12 +545,21 @@ class Privateer(object):
 
 def uuid(size=16):
     '''
-    Generate univerally unique id hex string with size characters
+    Generate universally unique id hex string with size characters
     Timebased with random bytes
     Minimum size is 16
+
+    Uses time.clock instead of time.time on windows.
+    Tests of rapid uuid generation fail to generate unique uuids
+    on Windows with time.time().
+    See http://www.pythoncentral.io/measure-time-in-python-time-time-vs-time-clock/
+    for discussion.
     '''
     size = max(int(size), 16)
-    front =  "{0:0x}".format(int(time.time() * 1000000)) # microseconds
+    if sys.platform == 'win32':
+        front =  "{0:0x}".format(int(time.clock() * 1000000)) # microseconds
+    else:
+        front =  "{0:0x}".format(int(time.time() * 1000000)) # microseconds
     extra = size - len(front)
     back = binascii.hexlify(libnacl.randombytes(extra // 2 + extra % 2))
     return ((front + back)[:size])
