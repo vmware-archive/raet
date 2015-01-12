1a946b6c93a032850ecfe05a6e648ca92e1902e7
Samuel M Smith <smith.samuel.m@gmail.com>
2015-01-12 11:46:21 -0700

    Fixed test runner so it is really starting correctly not implied start because default not exist

--- raet/test/__init__.py
+++ raet/test/__init__.py
@@ -21,16 +21,16 @@ from ioflo.base.consoling import getConsole
 console = getConsole()
 console.reinit(verbosity=console.Wordage.concise)
 
-def run(start="raet"):
+def run(start=None):
     '''
     Run unittests starting at directory given by start
     Default start is the location of the raet package
     '''
-    if __name__ == '__main__':
-        top = os.path.dirname(os.getcwd())
-    else:
-        top = os.path.dirname(os.path.dirname(os.path.abspath(
-                sys.modules.get(__name__).__file__)))
+    top = os.path.dirname(os.path.dirname(os.path.abspath(
+               sys.modules.get(__name__).__file__)))
+
+    if not start:
+        start = top
 
     console.terse("\nRunning all RAET tests in '{0}', starting at '{1}'\n".format(top, start))
     loader = unittest.TestLoader()
